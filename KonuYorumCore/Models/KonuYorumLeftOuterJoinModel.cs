32049dbf572c3c703746f53a4b4c1d17f65083d0
melik baykal <melikbilgeadam@outlook.com>
2022-02-27 19:43:27 +0300

    v1

---
+++ KonuYorumCore/Models/KonuYorumLeftOuterJoinModel.cs
@@ -0,0 +1,16 @@
+﻿namespace KonuYorumCore.Models
+{
+    public class KonuYorumLeftOuterJoinModel // KonuYorumLeftOuterJoinDTO, DTO: Data Transfer Object
+    {
+        #region Konu entitty'sinden gelen özelliler
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+        #endregion
+
+        #region Yorum entity'sinden gelen özellikler
+        public string Icerik { get; set; }
+        public string Yorumcu { get; set; }
+        public int? Puan { get; set; }
+        #endregion
+    }
+}
